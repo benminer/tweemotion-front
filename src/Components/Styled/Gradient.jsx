8036f2660a4585788438825e726f81b4dfa17330
Ben Miner <ben.miner@pop.belmont.edu>
2018-11-27 11:47:31 -0600

    animted gradient

--- src/Components/Styled/Gradient.jsx
+++ src/Components/Styled/Gradient.jsx
@@ -1,10 +1,32 @@
-import styled from 'styled-components/native';
-import { GRADIENT_LEFT, GRADIENT_RIGHT } from '../../Style/Colors';
+import styled from 'styled-components';
+// import { GRADIENT_LEFT, GRADIENT_RIGHT } from '../../Style/Colors';
 
-const Gradient = styled.ScrollView`
+const Gradient = styled.div`
     width: 100vw;
     height: 100vh;
-    background-image: linear-gradient(to bottom right, ${GRADIENT_LEFT}, ${GRADIENT_RIGHT});
+    overflow-y: scroll;
+    background: linear-gradient(270deg, #9d7b7d, #1bd5df);
+    background-size: 400% 400%;
+
+    -webkit-animation: AnimationName 20s ease infinite;
+    -moz-animation: AnimationName 20s ease infinite;
+    animation: AnimationName 20s ease infinite;
+
+    @-webkit-keyframes AnimationName {
+        0%{background-position:0% 50%}
+        50%{background-position:100% 50%}
+        100%{background-position:0% 50%}
+    }
+    @-moz-keyframes AnimationName {
+        0%{background-position:0% 50%}
+        50%{background-position:100% 50%}
+        100%{background-position:0% 50%}
+    }
+    @keyframes AnimationName { 
+        0%{background-position:0% 50%}
+        50%{background-position:100% 50%}
+        100%{background-position:0% 50%}
+    }
 `;
 
 export default Gradient;
